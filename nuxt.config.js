7bb4df5ee57ad8ef5c8eabf08383bcc5015590d5
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-04-04 13:59:41 -0700

    api test

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'https://suntechelectronics.tech/backend/wp-json/',
+    baseURL: 'http://suntechelectronics.tech/backend/wp-json/',
   },
 
   // sitemap: {
